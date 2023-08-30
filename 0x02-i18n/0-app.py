64530f64b45750debdc38fcb11380bcd8e4c7ae7
lavendahkinyua <lavendah17@gmail.com>
2023-08-30 07:21:24 +0300

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-""" Basic Flask app """
+""" Basic Flask app with a single GET route and html template """
 
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
